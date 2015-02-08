fa99c4e17661e9157b6f8f96749468273cc04784
AhmedFathiShaban <ahmed.1331@yahoo.com>
2015-02-08 08:05:54 +0200

    issue #20 , modified routes and modified groups controller : encapsulated some code into my_create_group_function and made sure an instructor can't have two groups with the samw name . also made the destroy action finds by name not id

--- Ruby-on-Rails-application/sQuiz/app/controllers/api/groups_controller.rb
+++ Ruby-on-Rails-application/sQuiz/app/controllers/api/groups_controller.rb
@@ -1,28 +1,93 @@
 class Api::GroupsController < ApplicationController
+  
+  acts_as_token_authentication_handler_for Instructor
 
-acts_as_token_authentication_handler_for Instructor
+  def add
 
-respond_to :json
+    student = Student.find_by_id(params[:student][:id])
+    group = Group.find_by_id(params[:group][:id])
 
-def Create
-
-  if instructor_signed_in?
-	
-	 group = Group.Create(Group_params)
-   group.instructor = current_instructor
-   if group.save
-
-	   render status: 200,
-            json: { success: true,
-                    info: "Group Created",
-                    data: { :group => group }}
-   else
-
+    if(!student)
       render status: :unprocessable_entity,
              json: { success: false,
-                     info: group.errors,
-                     data: {} }
-   end
+                        info: "Student does not exit",
+                        data: {} }
+
+    elsif(!group)
+      render status: :unprocessable_entity,
+             json: { success: false,
+                        info: "Group does not exit",
+                        data: {} }  
+
+    elsif (group.students.include?(student))
+      render status: :unprocessable_entity,
+             json: { success: false,
+                        info: "Student already exists in this group",
+                        data: {} }  
+        
+    elsif(group.instructor == current_instructor)
+
+      group.students << student
+      render status: 200,
+              json: { success: true,
+                      info: "Added",
+                      data: { instructor: current_instructor,
+                              student: student , group: group} }
+    else
+      render status: :unprocessable_entity,
+             json: { success: false,
+                        info: "Instructor is not authorized to add students to this group",
+                        data: {} }
+
+    end                    
+
+  end
+
+  def remove
+
+    student = Student.find(params[:student][:id])
+    group = Group.find(params[:group][:id])
+
+    if(group.instructor == current_instructor)
+
+      group.students.delete(student)
+      render status: 200,
+              json: { success: true,
+                      info: "Removed",
+                      data: { instructor: current_instructor,
+                              student: student , group: group} }
+    else
+      render status: :unprocessable_entity,
+             json: { success: false,
+                        info: "Instructor is not authorized to remove students to this group",
+                        data: {} }
+
+    end                    
+
+  end
+
+
+
+  def create
+
+  if instructor_signed_in?
+    
+   if(Group.find_by_name(params[:group][:name])==nil) 
+    my_create_group_function
+   else
+       if(Group.find_by_name(params[:group][:name]).instructor != current_instructor )
+            
+       my_create_group_function 
+
+       else
+
+       render status: 400,
+            json: { success: false,
+                    info: "You Can't make another group with the same name ",
+                   }
+
+       end 
+    end   
 
   else
 
@@ -36,9 +101,9 @@ render json: {
 end
 
 
-def Destroy
+def destroy
 
-    group = Group.find(params[:group][:id])
+    group = Group.find_by_name(params[:group][:name])
     if(group!=nil)
       if (current_instructor == group.instructor)
         group.destroy 
@@ -69,11 +134,34 @@ def Destroy
     end      
 end
 
+private
 
-def Group_params
+def group_params
     params.require(:group).permit(:name)
-end	
+end 
+
+def my_create_group_function
+
+            group = Group.create(group_params)
+            group.instructor = current_instructor
+            if group.save
+
+           render status: 200,
+                  json: { success: true,
+                    info: "Group Created",
+                    data: { :group => group }}
+            else
+
+            render status: :unprocessable_entity,
+                   json: { success: false,
+                     info: group.errors,
+                     data: {} }
+            end
+
+end  
 
 
 
-end	
+
+
+end
