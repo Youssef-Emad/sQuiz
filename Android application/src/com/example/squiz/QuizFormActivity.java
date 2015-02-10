ecdc9b19e4bd2e10a3950c184be05448dada422c
heshaam-abubakr <heshaam.abubakr@gmail.com>
2015-02-10 18:06:46 +0200

    Issue #64, quiz form

---
+++ Android application/src/com/example/squiz/QuizFormActivity.java
@@ -0,0 +1,15 @@
+package com.example.squiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuizFormActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_quiz_form);
+	}
+
+}
