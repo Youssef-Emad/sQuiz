cf462427a6b719a5873b67dad7295c4328931240
youssefMagdy29 <youssef.magdy29@gmail.com>
2015-02-10 21:26:23 +0200

    Issue #64, creating QuizDetailsActivity & its layout

---
+++ Android application/sQuiz/src/com/example/squiz/QuizDetailsActivity.java
@@ -0,0 +1,26 @@
+package com.example.squiz;
+
+import com.example.tabs.QuestionsPagerAdapter;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class QuizDetailsActivity extends FragmentActivity {
+	QuestionsPagerAdapter questionPagerAdapter;
+	ViewPager mViewPager;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizz_details);
+        questionPagerAdapter = new QuestionsPagerAdapter(getSupportFragmentManager());
+
+        final ActionBar actionBar = getActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(questionPagerAdapter);
+    }
+}
