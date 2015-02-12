c95cdfc3f2e749a2646153b70401a4a7850350d6
HaithamKhedr <haithamKhedr@gmail.com>
2015-02-12 18:05:48 +0200

    show Quizzes for a certain group

--- Android application/sQuiz/src/com/example/httpRequest/QuizApi.java
+++ Android application/sQuiz/src/com/example/httpRequest/QuizApi.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import retrofit.Callback;
 import retrofit.http.Body;
-import retrofit.http.DELETE;
 import retrofit.http.GET;
 import retrofit.http.Header;
 import retrofit.http.Headers;
@@ -18,11 +17,17 @@ import com.google.gson.JsonObject;
 public interface QuizApi {
 	@Headers({"Accept: application/json",})
 	@GET("/{type}/quizzes")
-	public void requestForm(@Header("X-Instructor-Email") String email,@Header("X-Instructor-Token") String token,@Path("type") String typ,Callback<List<Quiz>> callback);	
+	public void requestForm(@Header("X-Instructor-Email") String email,@Header("X-Instructor-Token") String token
+			,@Path("type") String typ,Callback<List<Quiz>> callback);	
 
 	@Headers({"Accept: application/json",})
 	@POST("/groups/delete")
-	public void deleteQuizzes(@Header("X-Instructor-Email") String email,@Header("X-Instructor-Token") String token,@Body  List<Quiz> quiz,Callback<JsonObject> callback);
+	public void deleteQuizzes(@Header("X-Instructor-Email") String email,@Header("X-Instructor-Token") String token
+			,@Body  List<Quiz> quiz,Callback<JsonObject> callback);
 
+	@Headers({"Accept: application/json",})
+	@GET("/groups/{id}/quizzes")
+	public void requestQuizzes(@Header("X-Instructor-Email") String email,@Header("X-Instructor-Token") String token
+			,@Path("id") int groupID,Callback<List<Quiz>> callback);
 	
 }
