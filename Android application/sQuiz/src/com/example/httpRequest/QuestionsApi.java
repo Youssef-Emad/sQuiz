c1593041f0cf7186cd646116ee3b28fd6b5a86b9
youssefMagdy29 <youssef.magdy29@gmail.com>
2015-02-13 18:12:19 +0200

    Issue #79, testing addquestions request

--- Android application/sQuiz/src/com/example/httpRequest/QuestionsApi.java
+++ Android application/sQuiz/src/com/example/httpRequest/QuestionsApi.java
@@ -5,6 +5,7 @@ import retrofit.http.Body;
 import retrofit.http.Header;
 import retrofit.http.Headers;
 import retrofit.http.POST;
+import retrofit.http.Path;
 
 import com.example.Models.Question;
 import com.google.gson.JsonObject;
@@ -12,8 +13,8 @@ import com.google.gson.JsonObject;
 
 public interface QuestionsApi {
 	@Headers({"Accept: application/json",})
-	@POST("/api/quizzes/addquestion/{id}")
+	@POST("/quizzes/addquestion/{id}")
 	public void addQuestions(@Header("X-Instructor-Email") String email,
-			@Header("X-Instructor-Token") String token, int id
+			@Header("X-Instructor-Token") String token, @Path("id") int id
 			,@Body Question[] questions,Callback<JsonObject> callback);
 }
--- Android application/sQuiz/src/com/example/instructor/QuizFormActivity.java
+++ Android application/sQuiz/src/com/example/instructor/QuizFormActivity.java
@@ -37,49 +37,49 @@ public class QuizFormActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz_form);
-		
+
 		numberOfMCQ = (EditText) findViewById(R.id.editTextNumberOfMcq);
 		numberOfRearrangement = (EditText) findViewById(R.id.editTextNumberOfRearrangement);
 		quizName = (EditText) findViewById(R.id.editTextQuizName);
 		quizsubj=(EditText) findViewById(R.id.editTextSubject);
 		Duration=(EditText) findViewById(R.id.editTextDuration);
-		
+
 		next = (Button) findViewById(R.id.next);
 		next.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				if(isOnline())
 				{	
-					 nMCQ =numberOfMCQ.getText().toString();
-				     nRe = numberOfRearrangement.getText().toString();
-					 QName = quizName.getText().toString();
-					 Qsubject=quizsubj.getText().toString();
-					 duration = Duration.getText().toString();
-						 quiz=new Quiz();
+					nMCQ =numberOfMCQ.getText().toString();
+					nRe = numberOfRearrangement.getText().toString();
+					QName = quizName.getText().toString();
+					Qsubject=quizsubj.getText().toString();
+					duration = Duration.getText().toString();
+					quiz=new Quiz();
 					try {
 						quiz.setQuizInfo(nMCQ, nRe, QName, Qsubject,duration);
 						RestAdapter restAdapter = new RestAdapter.Builder()
 						.setEndpoint(SignupActivity.ENDPOINT)  //call base url
-				    	.setLogLevel(LogLevel.FULL)
-				    	.build();
+						.setLogLevel(LogLevel.FULL)
+						.build();
 						SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(QuizFormActivity.this);
 						auth_token_string = settings.getString("authToken", "");
 						email=settings.getString("email", "");
 						task = restAdapter.create(QuizApi.class);
 						task.createQuiz(email, auth_token_string, quiz, new Callback<JsonObject>() {
-							
+
 							@Override
 							public void success(JsonObject arg0, Response arg1) {
 								String quizID=arg0.get("id").toString().replaceAll("\"", "");
 								Intent intent = new Intent(QuizFormActivity.this, QuizDetailsActivity.class);
 								intent.putExtra("quizID", Integer.parseInt(quizID));
-								intent.putExtra("nMCQ", nMCQ);
-								intent.putExtra("nRe", nRe);
+								intent.putExtra("nMCQ", Integer.parseInt(nMCQ));
+								intent.putExtra("nRe", Integer.parseInt(nRe));
 								intent.putExtra("quizName", QName);
 								startActivity(intent);
 							}
-							
+
 							@Override
 							public void failure(RetrofitError arg0) {
 								JsonObject obj =(JsonObject)arg0.getBody();
@@ -87,24 +87,24 @@ public class QuizFormActivity extends Activity {
 								Toast.makeText(QuizFormActivity.this, error, Toast.LENGTH_SHORT).show();
 							}
 						});
-					
+
 					} catch (Exception e) {
-					
+
 						Toast.makeText(QuizFormActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
 					}
 				}
-				
+
 			}
 		});
 	}
-	
+
 	private boolean isOnline() {
 		ConnectivityManager cm= (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo netActivity=cm.getActiveNetworkInfo();
 		if(netActivity !=null && netActivity.isConnectedOrConnecting())
 			return true;
-			else
-				return false;
+		else
+			return false;
 
 	} 
 
